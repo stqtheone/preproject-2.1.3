81e811ac941e91b219b0962cc213c2aa1a22187d
Alexandr <gub97@mail.ru>
2020-12-13 22:05:42 +0400

    commit

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -13,6 +13,9 @@ public class AnimalsCage {
     @Autowired
     private Timer timer;
 
+    public Timer getTimer(){
+        return timer;
+    }
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
